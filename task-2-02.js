67ce932023dceabd985571a9dbb6831128fe3c04
Myroslava Nikolaienko <kvi2nya@gmail.com>
2020-09-23 21:24:13 +0300

    Решена задача №02

--- task-2-02.js
+++ task-2-02.js
@@ -1,8 +1,15 @@
 const calculateEngravingPrice = (message = "", pricePerWord = 0) => {
     
-    const newLocal = this.split(/\s+/).length;
-    return newLocal;
+    const newLocal = message.split(/\s+/).length;
+    return newLocal*pricePerWord;
 }
 
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10)); // 80
 
-console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10));
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 20)); // 160
+
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40)); // 240
+
+console.log(calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20)); // 120
+
+console.log(calculateEngravingPrice('Uno', 100)); // 100
