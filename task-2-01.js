9f3532775ba17399e4c90e6ba594b4bf3f8a9409
Myroslava Nikolaienko <kvi2nya@gmail.com>
2020-09-23 18:49:01 +0300

    Создан файл для задачи №02

--- task-2-01.js
+++ task-2-01.js
@@ -1,34 +1,3 @@
-/* const getItemsString = function (array) {
-  'use strict';
-  let result;
-  let arrayElement = ('Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong');
-  const newLocal = array.length;
-  for (let i = 0; i < newLocal; i += 1) {
-    result = `${i} - ${arrayElement}`;
-    continue;
-  }
-  return (result);
-  
-};
-console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
- */
-
-/* newFunction();
-function newFunction() {
-    const getItemsString = function (array) {
-        'use strict';
-        let result;
-        let arrayElement = ('Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong');
-
-        for (let i = 0; i < array.length; i += 1) {
-            result = `${i} - ${arrayElement}`;
-            continue;
-        }
-        return (result);
-    };
-    console.log(getItemsString(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']));
-} */
-
 const getItemsString = function(array) {
   'use strict';
   let result = '';
---
+++ task-2-02.js
@@ -0,0 +1,8 @@
+const calculateEngravingPrice = (message = "", pricePerWord = 0) => {
+    
+    const newLocal = this.split(/\s+/).length;
+    return newLocal;
+}
+
+
+console.log(calculateEngravingPrice('Proin sociis natoque et magnis parturient montes mus', 10));
